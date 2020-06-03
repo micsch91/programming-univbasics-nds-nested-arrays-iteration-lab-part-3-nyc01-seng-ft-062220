364acbcdf9cde16d55114f205e188015d3b45a30
Michael Schneider <mic.sch91@gmail.com>
2020-06-03 16:47:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,28 +6,10 @@ def join_nested_strings(src)
 while row_index < src.count do
   element_index = 0
 while element_index < src[row_index].count do
-  total = src[row_index][element_index]
+  total = src[row_index][element_index].is_a?String
   element_index += 1
 end
 row_index += 1
 end
 join_nested_strings(src)
 end
-
-# guessing_game_grid = [
-#  [1, 2, 1, 7, 3],
-#  [2, 100, 15, 4, 18],
-#  [15, 16, 99, 1, 2, 11]
-# ]
-#
-# total = 0
-# row_index = 0
-# while row_index < guessing_game_grid.count do
-#   element_index = 0
-#   while element_index < guessing_game_grid[row_index].count do
-#     total += guessing_game_grid[row_index][element_index]
-#     element_index += 1
-#   end
-#   row_index += 1
-# end
-# total #=> 297
